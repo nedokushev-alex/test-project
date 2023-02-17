8599e6a08845a89ba004457a6d5cb01d540557ad
nedokushev <nedokushev@tochka.com>
2023-02-17 12:01:03 +0600

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * @author Aleksandr Nedokushev
+ * @created ${DATE}
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+}
